75cd528d699ec2b3c45e0ff7616886c74eaf6350
Baptiste Boucher <baptiste.boucher@gmail.com>
2018-05-16 16:46:24 +0200

    creation of the model and the index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 100 fake flats...'
+100.times do
+  flat = Flat.new(
+    name:    Faker::TwinPeaks.location,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Hipster.paragraph,
+    price_per_night: Faker::Commerce.price,
+    number_of_guests: [1, 2, 3, 4, 5, 6, 7].sample,
+  )
+  flat.save!
+end
+puts 'Finished!'
+
